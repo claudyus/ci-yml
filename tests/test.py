55b47d461eab9ea1e12bdfb533677deeb7cee00b
Claudio Mignanti <c.mignanti@gmail.com>
2015-04-16 08:15:21 +0200

    add tests

---
+++ tests/test.py
@@ -0,0 +1,37 @@
+import subprocess
+import unittest
+import shutil
+import os
+
+
+def rmfile(filename):
+    os.remove(filename) if os.path.exists(filename) else None
+
+class TestCmdLine(unittest.TestCase):
+
+    def test_00(self):
+        rmfile('/tmp/op1')
+        rmfile('/tmp/op2')
+        subprocess.check_output('ci-yml', shell=True)
+        assert os.path.exists('/tmp/op1')
+
+    def test_10(self):
+        """ test1 should raise exception """
+        rmfile('/tmp/fail_test1')
+        try:
+            subprocess.check_output('ci-yml test1', shell=True)
+        except:
+            assert os.path.exists('/tmp/fail_test1')
+
+    def test_20(self):
+        rmfile('/tmp/build_doc')
+        subprocess.check_output('ci-yml build_doc', shell=True)
+        assert os.path.exists('/tmp/build_doc')
+
+    def test_30(self):
+        rmfile('/tmp/deployed')
+        subprocess.check_output('ci-yml deploy', shell=True)
+        assert os.path.exists('/tmp/deployed')
+
+if __name__ == '__main__':
+    unittest.main()
